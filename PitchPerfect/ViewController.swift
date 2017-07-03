2fd0f728c826c87cad21caec15c39f74ddab9814
Erik Olson <ergoego@users.noreply.github.com>
2017-07-03 16:09:20 -0700

    Played with IBOutlets (Code -> UI) and IBActions (UI -> Code)

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
     
+    var isRecording = false // not a part of the project, but added because if this were real I would need a way to keep track of when recording has actually started/stopped. I need a variable to reference outside by some record method that actually calls upon the iPhone/iPad system resources to record audio.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,7 +26,14 @@ class ViewController: UIViewController {
 
     @IBAction func recordAudio(_ sender: Any) {
         print("record button was pressed")
+        isRecording = true //added to keep track of recording; not required from project
         recordingLabel.text = "Recording in Progress"
+    }
+    
+    @IBAction func stopRecordAudio(_ sender: Any) { //changing this func name broke it :( Have to delete and rewire to change name? Changing then re-wiring didn't work... have to destroy completely and rewire from scratch?
+        print("stop recording button was pressed")
+        isRecording = false //added to keep track of recording; not required from project
+        recordingLabel.text = "Tap to Record"
     }
 
 }
