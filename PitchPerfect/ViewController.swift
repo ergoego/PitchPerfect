53076b4c06849104fecc07aa104a2d4a3a772626
Erik Olson <ergoego@users.noreply.github.com>
2017-07-16 01:13:57 -0700

    asdf

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,30 +11,34 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
-    
-    var isRecording = false // not a part of the project, but added because if this were real I would need a way to keep track of when recording has actually started/stopped. I need a variable to reference outside by some record method that actually calls upon the iPhone/iPad system resources to record audio.
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    @IBOutlet weak var recordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func recordAudio(_ sender: Any) {
+   
+    @IBAction func recordAudio(_ sender: UIButton) {
         print("record button was pressed")
-        isRecording = true //added to keep track of recording; not required from project
         recordingLabel.text = "Recording in Progress"
-    }
-    
-    @IBAction func stopRecordAudio(_ sender: Any) { //changing this func name broke it :( Have to delete and rewire to change name? Changing then re-wiring didn't work... have to destroy completely and rewire from scratch?
-        print("stop recording button was pressed")
-        isRecording = false //added to keep track of recording; not required from project
-        recordingLabel.text = "Tap to Record"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
     }
 
+    @IBAction func stopRecordingAudio(_ sender: UIButton) {
+        recordingLabel.text = "Tap to Record"
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+    }
 }
 
